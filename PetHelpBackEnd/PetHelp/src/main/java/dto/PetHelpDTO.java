e2512271b9ab81bd8d61d719001ffd2e46caaffe
LenitaM <lenitamaciel.ti@gmail.com>
2024-02-29 22:11:16 -0300

    domain, repository e dto ajustes

--- PetHelpBackEnd/PetHelp/src/main/java/dto/PetHelpDTO.java
+++ PetHelpBackEnd/PetHelp/src/main/java/dto/PetHelpDTO.java
@@ -1,5 +1,54 @@
 package dto;
 
-public class PetHelpDTO {
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
+public class PetHelpDTO {
+ 
+    @NotBlank(message = "O campo Nome é obrigatório")
+	protected String nome;
+	
+	@Email(message = "Email inválido")
+	@NotBlank(message = "O campo Email é obrigatório")
+	protected String email;
+	
+	@NotBlank(message = "O campo Telefone é obrigatório")
+	protected String telefone;
+	
+	@NotBlank(message = "O campo Senha é obrigatório")
+	protected String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		 return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getTelefone() {
+		 return telefone;
+	}
+	
+	public void setTelefone( String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getSenha() {
+		 return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.telefone = senha;
+	}
 }
--- PetHelpBackEnd/PetHelp/src/main/java/repositories/PetHelpRepository.java
+++ PetHelpBackEnd/PetHelp/src/main/java/repositories/PetHelpRepository.java
@@ -1,5 +1,10 @@
 package repositories;
 
-public interface PetHelpRepository {
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.PetHelpBackEnd.domain.PetHelp;
+
+@Repository
+public interface PetHelpRepository extends JpaRepository<PetHelp, Integer> {
 
 }
